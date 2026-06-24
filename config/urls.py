eb3d96fe44a15f8196e18742c0ccbdbb3d1dc94e
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-06-24 21:32:03 +0530

    Create urls.py

---
+++ config/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import path
+
+
+def home(request):
+    return HttpResponse("Darjeeling Homestay Platform is running successfully!")
+
+
+urlpatterns = [
+    path("", home),
+    path("admin/", admin.site.urls),
+]
