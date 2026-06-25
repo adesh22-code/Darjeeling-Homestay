3038c54d6798d3e9c73bcdc6d2d99d8791586f40
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-06-25 21:16:17 +0530

    Update Views.py

--- homestays/Views.py
+++ homestays/Views.py
@@ -1,1 +1,14 @@
+from django.shortcuts import render
+from .models import Homestay
 
+
+def home(request):
+    homestays = Homestay.objects.all()
+
+    return render(
+        request,
+        "homestays/home.html",
+        {
+            "homestays": homestays
+        }
+    )
