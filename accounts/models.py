6078d4d41733845cfa316e5c4b7a025837792783
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-06-25 12:08:26 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,25 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-USER_TYPES = (
-("customer", "Customer"),
-("owner", "Owner"),
-)
+    USER_TYPES = (
+        ("customer", "Customer"),
+        ("owner", "Owner"),
+    )
 
-```
-user_type = models.CharField(
-    max_length=20,
-    choices=USER_TYPES,
-    default="customer",
-)
+    user_type = models.CharField(
+        max_length=20,
+        choices=USER_TYPES,
+        default="customer"
+    )
 
-phone = models.CharField(
-    max_length=15,
-    blank=True,
-    null=True,
-)
+    phone = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True
+    )
 
-def __str__(self):
-    return self.username
-```
+    def __str__(self):
+        return self.username
