6f0c982a04aed8fea4141b2573ae54044d0487cc
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-06-27 11:29:11 +0530

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 def home(request):
@@ -11,3 +13,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("homestays.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
