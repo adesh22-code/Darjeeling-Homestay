1041ef9f97c56fb7f9a19c78764616ab8a9d9aad
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-06-27 21:15:23 +0530

    Update models.py

--- homestays/models.py
+++ homestays/models.py
@@ -6,6 +6,19 @@ class Homestay(models.Model):
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
     available_rooms = models.IntegerField()
     image = models.ImageField(upload_to="homestays/", blank=True, null=True)
+    description = models.TextField(blank=True)
+
+    bedrooms = models.PositiveIntegerField(default=1)
+
+    bathrooms = models.PositiveIntegerField(default=1)
+
+    max_guests = models.PositiveIntegerField(default=2)
+
+    wifi = models.BooleanField(default=True)
+
+    parking = models.BooleanField(default=False)
+
+    breakfast = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
