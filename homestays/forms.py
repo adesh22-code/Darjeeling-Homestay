885e9d09dd328533a88b17ffcdfe1d3762c63b4e
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-06-28 10:47:21 +0530

    Create forms.py

---
+++ homestays/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Booking
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = [
+            "guest_name",
+            "guest_email",
+            "guest_phone",
+            "check_in",
+            "check_out",
+            "guests",
+        ]
+
+        widgets = {
+            "check_in": forms.DateInput(attrs={"type": "date"}),
+            "check_out": forms.DateInput(attrs={"type": "date"}),
+        }
