9af4c408054bfbb39954d3dc0ff95fca35a998dc
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-06-28 10:49:18 +0530

    Update views.py

--- homestays/views.py
+++ homestays/views.py
@@ -1,5 +1,6 @@
 from .models import Homestay
 from django.shortcuts import render, get_object_or_404
+from .forms import BookingForm
 
 
 def home(request):
@@ -12,6 +13,8 @@ def home(request):
             "homestays": homestays
         }
     )
+
+
 def homestay_detail(request, id):
     homestay = get_object_or_404(Homestay, id=id)
 
@@ -20,5 +23,37 @@ def homestay_detail(request, id):
         "homestays/detail.html",
         {
             "homestay": homestay
+        }
+    )
+
+
+def homestay_detail(request, id):
+    homestay = get_object_or_404(Homestay, id=id)
+
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.homestay = homestay
+            booking.save()
+
+            return render(
+                request,
+                "homestays/booking_success.html",
+                {
+                    "booking": booking
+                }
+            )
+
+    else:
+        form = BookingForm()
+
+    return render(
+        request,
+        "homestays/detail.html",
+        {
+            "homestay": homestay,
+            "form": form,
         }
     )
