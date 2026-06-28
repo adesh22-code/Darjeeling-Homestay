ba123af55480d3ec02575a0c0ef4fdad11713a02
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-06-28 15:43:23 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("register/", views.register, name="register"),
+    path("login/", views.customer_login, name="login"),
+    path("logout/", views.customer_logout, name="logout"),
 ]
