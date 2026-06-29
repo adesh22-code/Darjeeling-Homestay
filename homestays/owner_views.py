aec86b6f6a0fdfffb79da793834522db82c31d36
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-06-29 15:39:51 +0530

    Update owner_views.py

--- homestays/owner_views.py
+++ homestays/owner_views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms_owner import HomestayForm
 
 from .models import Homestay
