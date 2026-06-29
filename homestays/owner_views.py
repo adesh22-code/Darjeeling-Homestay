b66b2c160109328049da9165948bcbb0a573088b
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-06-29 12:09:22 +0530

    Update owner_views.py

--- homestays/owner_views.py
+++ homestays/owner_views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms_owner import HomestayForm
 
 from .models import Homestay
 
@@ -21,5 +22,46 @@ def owner_dashboard(request):
         "homestays/owner_dashboard.html",
         {
             "homestays": homestays,
+        },
+    )
+
+
+
+@login_required
+def add_homestay(request):
+
+    if request.user.user_type != "owner":
+        return render(
+            request,
+            "403.html",
+            status=403,
+        )
+
+    if request.method == "POST":
+
+        form = HomestayForm(
+            request.POST,
+            request.FILES,
+        )
+
+        if form.is_valid():
+
+            homestay = form.save(commit=False)
+
+            homestay.owner = request.user
+
+            homestay.save()
+
+            return redirect("owner_dashboard")
+
+    else:
+
+        form = HomestayForm()
+
+    return render(
+        request,
+        "homestays/add_homestay.html",
+        {
+            "form": form,
         },
     )
