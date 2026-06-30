0ae49cbe112865e13cf1fe44675d15b57a9f681a
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-06-30 23:52:45 +0530

    Update views.py

--- homestays/views.py
+++ homestays/views.py
@@ -1,5 +1,5 @@
 from .models import Homestay, Booking
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import BookingForm
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
